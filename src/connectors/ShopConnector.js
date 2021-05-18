e116325dea0e27a6aa7467e2ca8f5de39be694cc
sturgeon.landon <sturgeon.landon@gmail.com>
2021-05-17 21:38:27 -0800

    actually added admin component this time

--- src/connectors/ShopConnector.js
+++ src/connectors/ShopConnector.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import * from ShopActions from "../data/ActionCreaters";
+import * as ShopActions from "../data/ActionCreaters";
 import DataTypes from "../data/Types";
 import Shop from "../components/shop/Shop";
 import * as CartActions from "../data/CartActionCreators";
@@ -12,13 +12,17 @@ import Thanks from "../components/shop/Thanks";
 
 const mapDispatchToProps = { ...ShopActions, ...CartActions };
 
-const ShopConnector = connect(ds => ds, mapDispatchToProps) (
+const ShopConnector = connect(
+  (ds) => ds,
+  mapDispatchToProps
+)(
   class extends Component {
     selectComponent = (routeProps) => {
-      const wrap = (Component, Content) =>
+      const wrap = (Component, Content) => (
         <Component {...this.props} {...routeProps}>
-          { Content && wrap(Content)}
+          {Content && wrap(Content)}
         </Component>
+      );
       switch (routeProps.match.params.section) {
         case "products":
           return wrap(DataGetter, Shop);
@@ -29,18 +33,27 @@ const ShopConnector = connect(ds => ds, mapDispatchToProps) (
         case "thanks":
           return wrap(Thanks);
         default:
-          return <Redirect to="/shop/products/all/1" />
+          return <Redirect to="/shop/products/all/1" />;
       }
-    }
+    };
 
     render() {
-      return <Switch>
-        <Redirect from="/shop/products/:category" to="/shop/products/:category/1" exact={ true } />
-        <Redirect path={"/shop:section?/:category?/:page?" } render={routeProps => this.selectComponent(routeProps)} />
-      </Switch>
+      return (
+        <Switch>
+          <Redirect
+            from="/shop/products/:category"
+            to="/shop/products/:category/1"
+            exact={true}
+          />
+          <Redirect
+            path={"/shop:section?/:category?/:page?"}
+            render={(routeProps) => this.selectComponent(routeProps)}
+          />
+        </Switch>
+      );
     }
     componentDidMount = () => this.props.loadData(DataTypes.CATEGORIES);
   }
-)
+);
 
 export default ShopConnector;
